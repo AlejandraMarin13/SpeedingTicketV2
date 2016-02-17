b5efb45f52e96640ad887067cc8c90f715c2d419
AlejandraMarin13 <jamarin393@misena.edu.co>
2016-02-17 00:55:01 -0500

    SE CREAN LAS CLASES CUENTA Y EQUIPO DAO

---
+++ SpeedingTicket/src/edu/co/sena/dao/CuentaDAO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.dao;
+
+import edu.co.sena.modelo.dto.Cuenta;
+import edu.co.sena.modelo.dto.CuentaPk;
+import java.util.List;
+
+/**
+ *
+ * @author PCOPEN
+ */
+public interface CuentaDAO {
+    
+     public List<Cuenta> findAll();
+
+    public void insert(Cuenta dto);
+
+    public void update(CuentaPk llaveDto, Cuenta dto);
+
+    public void updatePk(CuentaPk viejo, CuentaPk nuevo);
+
+    public void delete(CuentaPk dot);
+
+    public List<Cuenta> findByPK(CuentaPk dto);
+
+    public int count();
+}
+
+
---
+++ SpeedingTicket/src/edu/co/sena/dao/EquipoDAO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.dao;
+
+import edu.co.sena.modelo.dto.Equipo;
+import edu.co.sena.modelo.dto.EquipoPk;
+import java.util.List;
+
+/**
+ *
+ * @author PCOPEN
+ */
+public interface EquipoDAO {
+   
+    public abstract String getTableName();
+
+    public abstract List<Equipo> findAll();
+
+    public abstract void insert(Equipo equipoDTO);
+
+    public abstract void update(Equipo equipoDTO);
+
+    public void updatePK(EquipoPk nuevo, EquipoPk viejo);
+}
+ 
+
