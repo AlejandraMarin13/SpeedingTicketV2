aa7df5105334fcf3b2b7ee9b4c36e7ddf365e565
johanavalverde <ajvalverde@misena.edu.co>
2016-02-16 22:13:46 -0500

    Se Areglo La Clase ResourseManager

--- SpeedingTicket/src/edu/co/sena/modelo/dao/mysql/ResourseManager.java
+++ SpeedingTicket/src/edu/co/sena/modelo/dao/mysql/ResourseManager.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import static java.sql.DriverManager.getConnection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -17,20 +18,20 @@ import java.sql.ResultSet;
  */
 public class ResourseManager {
   
-    private static String JDBC_URL = "jdbc:mysql://localhost:3306/proyecto";
+    private static String JDBC_URL = "jdbc:mysql:/localhost:3306/proyecto";
     private static String JDBC_USER = "root";
     private static String JDBC_PASSWORD = "123456789";
 
-    public static Connection getConneccion() {
-
+    public static synchronized Connection getConection() {
         Connection conexion = null;
         try {
             conexion = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
         } catch (Exception e) {
-            System.err.println("No hay conexion" + e.getMessage());
+            System.err.println("Falla De La Conexion" + e.getMessage());
 
         }
         return conexion;
+
     }
 
     public static void closeConnection(Connection con) {
@@ -39,23 +40,22 @@ public class ResourseManager {
                 con.close();
             }
         } catch (Exception e) {
+            System.err.println("Error En Cerrar La Conexion" + e.getMessage());
 
-            System.err.println("Error al cerrar la conexion" + e.getMessage());
         }
-
     }
-    public static void closeResultSet(ResultSet rs) {
+
+    public static void close(ResultSet rs) {
         try {
             if (rs != null) {
                 rs.close();
             }
-        } catch (Exception e) {
-
-            System.err.println("Error al cerrar el resultset" + e.getMessage());
+        } catch (SQLException sqle) {
+            System.out.println("");
         }
 
     }
-    public static void closePreparedStatement(PreparedStatement sentencia) {
+     public static void closePreparedStatement(PreparedStatement sentencia) {
         try {
             if (sentencia != null) {
                 sentencia.close();
@@ -65,7 +65,9 @@ public class ResourseManager {
             System.err.println("Error al cerrar el prepared statement" + e.getMessage());
         }
 
-    } 
+    }
+
+   
    
 
 }
