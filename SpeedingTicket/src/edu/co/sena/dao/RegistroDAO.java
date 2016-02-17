d75eae6aa6b51d100593921a8f34abf3dadd7490
johanavalverde <ajvalverde@misena.edu.co>
2016-02-17 01:28:58 -0500

    Se Creo La Interface RegistroDAO

---
+++ SpeedingTicket/src/edu/co/sena/dao/RegistroDAO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.dao;
+
+import edu.co.sena.modelo.dto.Registro;
+import edu.co.sena.modelo.dto.RegistroPk;
+import java.util.List;
+
+/**
+ *
+ * @author ADMIN
+ */
+public interface RegistroDAO {
+    
+    public List<Registro> findAll();
+
+    public void insert(Registro dto);
+
+    public void update(RegistroPk llaveDto, Registro dto);
+
+    public void updatePk(RegistroPk llaveVieja, RegistroPk llaveNueva);
+
+    public void delete(RegistroPk dto);
+
+    public List<Registro> findByPK(RegistroPk dto);
+
+    public int count();
+}
+
+     
+
+    
+
