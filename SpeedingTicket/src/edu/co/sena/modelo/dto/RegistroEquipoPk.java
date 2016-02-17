07e110659422ef4fc8182c4d6d849e134e0cff0e
LINDA <LINDA@LINDA-PC>
2016-02-16 23:17:08 -0500

    Se creo RegistroEquipoPk

---
+++ SpeedingTicket/src/edu/co/sena/modelo/dto/RegistroEquipoPk.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.modelo.dto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author LINDA
+ */
+public class RegistroEquipoPk implements Serializable {
+
+    private String propietarioEquipoIdEquipo;
+     private String propietarioTipoDocumento;
+    private String propietarioCuentaNumeroDocumento;
+
+    public String getPropietarioEquipoIdEquipo() {
+        return propietarioEquipoIdEquipo;
+    }
+
+    public void setPropietarioEquipoIdEquipo(String propietarioEquipoIdEquipo) {
+        this.propietarioEquipoIdEquipo = propietarioEquipoIdEquipo;
+    }
+
+    public String getPropietarioTipoDocumento() {
+        return propietarioTipoDocumento;
+    }
+
+    public void setPropietarioTipoDocumento(String propietarioTipoDocumento) {
+        this.propietarioTipoDocumento = propietarioTipoDocumento;
+    }
+
+    public String getPropietarioCuentaNumeroDocumento() {
+        return propietarioCuentaNumeroDocumento;
+    }
+
+    public void setPropietarioCuentaNumeroDocumento(String propietarioCuentaNumeroDocumento) {
+        this.propietarioCuentaNumeroDocumento = propietarioCuentaNumeroDocumento;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistroEquipoPk{" + "propietarioEquipoIdEquipo=" + propietarioEquipoIdEquipo + ", propietarioTipoDocumento=" + propietarioTipoDocumento + ", propietarioCuentaNumeroDocumento=" + propietarioCuentaNumeroDocumento + '}';
+    }
+   
+
+}
